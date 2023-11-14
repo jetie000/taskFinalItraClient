8946a1e1ceb35ccf17df2cb9fd3b5ac08a892327
jetie000 <makeenko.005@gmail.com>
2023-11-14 21:34:27 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.scss";
+
+const App = () => (
+  <div className="container">
+    <div>Name: final-task-itra-client</div>
+    <div>Framework: react</div>
+    <div>Language: TypeScript</div>
+    <div>CSS: Empty CSS</div>
+  </div>
+);
+ReactDOM.render(<App />, document.getElementById("app"));
